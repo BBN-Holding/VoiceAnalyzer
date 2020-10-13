e0d5dbca4075e89801c0548d2e2b74664bd20faa
Schlauer-Hax <arzberger52@gmail.com>
2020-10-13 23:18:52 +0200

    Fix

--- src/main/java/com/bbn/voiceanalyzer/CommandListener.java
+++ src/main/java/com/bbn/voiceanalyzer/CommandListener.java
@@ -22,50 +22,59 @@ public class CommandListener extends ListenerAdapter {
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
         if (event.getMessage().getContentRaw().equals("-statstop")) {
             HashMap<JSONObject, Long> times = new HashMap<>();
-            event.getGuild().loadMembers();
-            for (JSONObject jsonObject : rethink.getAll(event.getGuild().getIdLong())) {
-                Date lastlefttime = new Date();
+            event.getGuild().loadMembers().onSuccess(
+                    members -> {
+                        for (JSONObject jsonObject : rethink.getAll(event.getGuild().getIdLong())) {
+                            Date lastlefttime = new Date();
 
-                long lastconnectedtime = Long.parseLong(jsonObject.getString("lastConnectedTime"));
+                            long lastconnectedtime = Long.parseLong(jsonObject.getString("lastConnectedTime"));
 
-                String connected = jsonObject.getString("connected");
-                long connectednew = 0;
-                if (lastconnectedtime != 0) {
-                    connectednew = lastlefttime.getTime() - lastconnectedtime;
-                }
+                            String connected = jsonObject.getString("connected");
+                            long connectednew = 0;
+                            if (lastconnectedtime != 0) {
+                                connectednew = lastlefttime.getTime() - lastconnectedtime;
+                            }
 
-                long elapsedTime = Long.parseLong(connected) + connectednew;
+                            long elapsedTime = Long.parseLong(connected) + connectednew;
 
-                times.put(jsonObject, elapsedTime);
+                            times.put(jsonObject, elapsedTime);
 
-            }
+                        }
 
-            Set<Map.Entry<JSONObject, Long>> set = times.entrySet();
-            List<Map.Entry<JSONObject, Long>> list = new ArrayList<>(set);
-            list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
-            StringBuilder sb = new StringBuilder();
-            for (Map.Entry<JSONObject, Long> entry : list) {
-                JSONObject json = entry.getKey();
+                        Set<Map.Entry<JSONObject, Long>> set = times.entrySet();
+                        List<Map.Entry<JSONObject, Long>> list = new ArrayList<>(set);
+                        list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+                        ArrayList<String> strings = new ArrayList<>();
+                        for (Map.Entry<JSONObject, Long> entry : list) {
+                            if (list.indexOf(entry)>10) break;
+                            JSONObject json = entry.getKey();
 
-                long elapsedSeconds = entry.getValue() / 1000;
-                long secondsDisplay = elapsedSeconds % 60;
-                long elapsedMinutes = elapsedSeconds / 60;
-                long minutesDisplay = elapsedMinutes % 60;
-                long elapsedHours = elapsedMinutes / 60;
-                long hoursDisplay = elapsedHours % 24;
-                long elapsedDays = elapsedHours / 24;
+                            long elapsedSeconds = entry.getValue() / 1000;
+                            long secondsDisplay = elapsedSeconds % 60;
+                            long elapsedMinutes = elapsedSeconds / 60;
+                            long minutesDisplay = elapsedMinutes % 60;
+                            long elapsedHours = elapsedMinutes / 60;
+                            long hoursDisplay = elapsedHours % 24;
+                            long elapsedDays = elapsedHours / 24;
 
-                event.getJDA().retrieveUserById(json.getString("memberid")).queue(
-                        user -> sb.append(user.getAsTag()).append(" - ").append(String.format("%02d Days %02d:%02d:%02d", elapsedDays, hoursDisplay, minutesDisplay, secondsDisplay)).append("\n")
-                );
-            }
-            event.getChannel().sendMessage(new EmbedBuilder()
-                    .setTitle("Voice Toplist")
-                    .setDescription(sb.toString())
-                    .setFooter("Provided by BBN", "https://bigbotnetwork.com/images/avatar.png")
-                    .setTimestamp(Instant.now())
-                    .build()).queue();
-        } else if(event.getMessage().getContentRaw().startsWith("-stats")) {
+                            event.getJDA().retrieveUserById(json.getString("memberid")).queue(
+                                    user -> {
+                                        strings.add(user.getAsTag() + " - " + String.format("%02d Days %02d:%02d:%02d", elapsedDays, hoursDisplay, minutesDisplay, secondsDisplay));
+                                        if (strings.size()==list.size() || list.size()>10 && strings.size()==10) {
+                                            event.getChannel().sendMessage(new EmbedBuilder()
+                                                    .setTitle("Voice Toplist")
+                                                    .setDescription(String.join("\n", strings.toArray(String[]::new)))
+                                                    .setFooter("Provided by BBN", "https://bigbotnetwork.com/images/avatar.png")
+                                                    .setTimestamp(Instant.now())
+                                                    .build()).queue();
+                                        }
+                                    }
+                            );
+                        }
+                    }
+            );
+
+        } else if (event.getMessage().getContentRaw().startsWith("-stats")) {
 
             Member member = event.getMember();
             if (event.getMessage().getMentionedMembers().size() == 1)
